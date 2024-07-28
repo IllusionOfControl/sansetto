293b46e41ffa94294add8b954acaa8ae5c4a445d
Sergey Skorokhod <sergeyskorokhod2@gmail.com>
2024-07-28 13:25:31 +0300

    clean root directory

--- app/extensions.py
+++
@@ -1,7 +0,0 @@
-from flask_apscheduler import APScheduler
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-
-scheduler = APScheduler()
-db = SQLAlchemy()
-migrate = Migrate()
--- app/utils.py
+++
@@ -1,8 +0,0 @@
-accepted_mimetypes = [
-    "image/jpeg",
-    "image/png",
-]
-
-
-def check_accepted_mimetype(mimetype: str) -> bool:
-    return mimetype in accepted_mimetypes
